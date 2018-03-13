ea9fad52a3e3ced97ca87f91584f7817831cfc5b
Haonan <32566885+yuhaonankaka@users.noreply.github.com>
2018-03-13 22:53:27 +0100

    some other basic exercises

---
+++ FirstJavascript/Stalker.js
@@ -0,0 +1,5 @@
+var UserFname = prompt("What's your first name?");
+var UserLname = prompt("What's your last name?");
+var UserAge = prompt("How old are you?");
+console.log("Your full name is: " + UserFname+" "+UserLname);
+console.log("Your age is: "+UserAge);
---
+++ FirstJavascript/age.js
@@ -0,0 +1,3 @@
+var age = prompt("How old are you?");
+console.log("You've been alive for " + age*365.2422 +" days!");
+alert("You've been alive for " + age*365.2422 +" days!");
