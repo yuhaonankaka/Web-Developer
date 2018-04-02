95a50db704f387ee08b135501d80560149d87b5b
Haonan <32566885+yuhaonankaka@users.noreply.github.com>
2018-04-02 11:29:49 +0200

    selection exercises

---
+++ DOM selector/script.js
@@ -0,0 +1,13 @@
+var h1 = document.getElementById("first");
+console.log(h1);
+document.querySelector("#first");
+
+document.getElementsByClassName("special");
+document.getElementsByClassName("special")[0];
+document.querySelector(".special");
+document.querySelectorAll(".special")[0];
+document.getElementsByTagName("p");
+document.getElementsByTagName("p")[0];
+document.querySelector("p");
+document.querySelectorAll("p")[0]
+document.querySelector("h1 + p");
