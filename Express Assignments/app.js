8ee2cb65a51c3f73f10929d28e0a2e23abb44881
Haonan <32566885+yuhaonankaka@users.noreply.github.com>
2018-04-14 17:45:13 +0200

    Expressjs Assignments

---
+++ Express Assignments/app.js
@@ -0,0 +1,24 @@
+var express = require("express");
+var app = express();
+
+
+app.get("/", function(req,res){
+    res.send("Hi There!!");
+});
+
+app.get("/bye", function(req,res){
+    res.send("Goodbye");
+});
+
+app.get("/dog", function(req,res){
+    res.send("Wang!!!");
+});
+
+
+app.get("*", function(req, res) {
+    res.send("You are a star!");
+});
+
+app.listen(process.env.PORT,process.env.IP,function(){
+    console.log("Sever has started!");
+});
